b73a4be406e62fa0f9b7159bf2335c5f9dd552b5
Cole Bish <colester32@yahoo.com>
2021-09-21 21:37:43 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+// import { useState } from 'react';
 import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+// https://pokeapi.co/api/v2/pokemon
 
 function App() {
+  const [apiData, setApiData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807").then((res) => { console.log(res); setApiData(res.data.results); }).catch((err) => console.log(err))
+  }, [])
+
+
+  // axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => console.log(res)).catch((err) => console.log(err))
+
+
+  // const Example = (props) => {
+  //   const [people, setPeople] = useState([]);
+
+  //   useEffect(() => {
+  //     fetch('https://pokeapi.co/api/v2/pokemon/')
+  //       .then(response => response.json())
+  //       .then(response => setPeople(response.results))
+  //   }, []);
+
+  //   return (
+  //     <div>
+  //       {people.length > 0 && people.map((person, index) => {
+  //         return (<div key={index}>{person.name}</div>)
+  //       })}
+  //     </div>
+  //   );
+  // }
+
+
+
+  // const [response, setResponse] = useState([])
+  // let api = fetch("https://pokeapi.co/api/v2/pokemon");
+  // setResponse(api);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <p>{response}</p> */}
+      {apiData.length > 0 && apiData.map((element, index) => (
+        <div key={index}>
+          <p>{element.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
